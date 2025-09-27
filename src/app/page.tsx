71356ccb606f847c0d092fb9d9127e4ff560a0b9
mantelfahrer <janschwarz30@gmail.com>
2025-09-27 14:53:29 +0200

    fetch list of pokemon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { List } from "./components/list";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
+  const pokemonlist = await data.json();
+  console.log(pokemonlist);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Pokemon List</h1>
